05b449ab51c4392d6cb798556e5d7dbc0c24da46
SagarCH <sagarch.aaro@gmail.com>
2018-08-09 07:07:13 +0530

    GmailLogin page

---
+++ JenkinsDemo/src/aug1/GmailLogin.java
@@ -0,0 +1,29 @@
+package aug1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class GmailLogin {
+	@Test
+	public static void test() throws Exception{
+		System.setProperty("webdriver.chrome.driver", "D:\\Learning\\Drivers\\Chrome\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://accounts.google.com/ServiceLogin/identifier?service=mail&flowName=GlifWebSignIn&flowEntry=AddSession");
+		System.out.println("Gmail website is launched");
+		driver.findElement(By.id("identifierId")).sendKeys("Seleniumlearning0806@gmail.com");
+		System.out.println("email id is entered in text-box");
+		
+		driver.findElement(By.xpath("*//span[text()='Next']")).click();
+		System.out.println("Next button is clicked");
+		Thread.sleep(5000);
+		driver.findElement(By.name("password")).sendKeys("SeleniumTest");
+		System.out.println("password is entered in text-box");
+		driver.findElement(By.xpath("*//span[text()='Next']")).click();
+		System.out.println("next button is clicked");
+		Thread.sleep(10000);
+		driver.quit();
+	
+	}
+}
